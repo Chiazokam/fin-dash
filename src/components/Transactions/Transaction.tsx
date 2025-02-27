51d0cb3fb75bfca84dd7c330e7609579ca05b0ee
Chiazokam Echeta <chiazokamecheta@gmail.com>
2025-02-27 23:55:55 +0100

    feat: implement recent transactions

--- src/components/Transactions/Transaction.tsx
+++ src/components/Transactions/Transaction.tsx
@@ -1,21 +1,24 @@
-import { Paypal } from "../Icons"
+import { Paypal, Credit, CardDeposit } from "../Icons"
 import type { TransactionType } from '../../store/reducers/transactionSlice';
 import { getTransactionDate, cn } from "../../utils"; 
 
+// Used this to randomly assign icons to the transactions. In real life, this could come with the transaction data
+const TransactionIcons = [Credit, Paypal, CardDeposit]
 interface Props {
     transaction: TransactionType
 }
 
 export const Transaction = ({ transaction }: Props) => {
     const { transactionTitle, transactionType, createdAt, amount } = transaction;
+    const Icon = TransactionIcons[Math.floor(Math.random()*3)]; // Different icons on every render
 
     return (
         <div className="flex justify-between items-center">
             <div className="flex gap-[17px] items-center">
                 
-                <Paypal />
+                <Icon />
                 <div className="flex flex-col flex-1">
-                    <p className="text-sm md:text-base text-black font-medium w-40 truncate">{transactionTitle}</p>
+                    <p className="text-sm md:text-base text-black font-medium w-120 md:w-40 truncate">{transactionTitle}</p>
                     <p className="text-secondary text-xs md:text-[15px] font-normal">{getTransactionDate(createdAt)}</p>
                 </div>
             </div>
